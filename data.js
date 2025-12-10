1cd1f38702520a61487f26a2114fd963d2c8d97d
ingyucarry <maxyeo050513@gmail.com>
2025-12-10 09:52:35 +0900

    데이터 파일 생성 및 STORE_DATA 추가

---
+++ data.js
@@ -0,0 +1,7 @@
+// data.js
+const STORE_DATA = [
+    { category: "분식", name: "명가떡볶이", sub: "떡볶이, 김밥", price: "1만원 이하" },
+    { category: "분식", name: "참새방앗간", sub: "즉석 떡볶이", price: "1만원 이하" },
+    { category: "분식", name: "셀분식", sub: "종합분식", price: "1만원 이하" },
+    { category: "분식", name: "신전떡볶이", sub: "떡볶이, 김밥", price: "1만원 이하" }
+];
